4d6b53f1fe16923e3372892c5f919c151e2e6166
JEGIN <jeginkumar5@gmail.com>
2024-03-18 12:20:54 +0530

    Offer cards

--- lib/src/views/dashboard/restaurant_detailed_screen.dart
+++ lib/src/views/dashboard/restaurant_detailed_screen.dart
@@ -24,10 +24,7 @@ class _RestaurantDetailsScreenWidgetState
     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
     'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
     'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-  ];
+   ];
   @override
   Widget build(BuildContext context) {
     final List<Widget> imageSliders = imgList
@@ -41,21 +38,54 @@ class _RestaurantDetailsScreenWidgetState
               ),
               borderRadius: BorderRadius.all(Radius.circular(20)),
             ),
-            child: Row(
+          
+          child: Padding(padding: EdgeInsets.all(10),child:   Column(
               children: [
-                Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    
-                  ],
-                ),
+                    Image.asset( 'assets/image/swiggy.png',height: 50,width: 50,),
+                
                 Column(
-                  children: [],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Flat ₹ 15 off',style:TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
+                    Text('No code required | Above ₹ 500'.toUpperCase(),style:TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    )),
+                  ]),]),
+                     SizedBox(height: 10,),
+                   Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _offerController.animateToPage(entry.key),
+                child:
+                Padding(padding: EdgeInsetsDirectional.fromSTEB(3, 0, 3, 0),
+               child:  Container(
+                  width: 8.0,
+                  height: 8.0,
+                 
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: (Theme.of(context).brightness == Brightness.dark
+                              ? Colors.white
+                              : Colors.black)
+                          .withOpacity(_offerCurrent == entry.key ? 0.9 : 0.4)),
                 ),
-                Column(
-                  children: [],
-                )
+              ));
+            }).toList(),
+          ),
+              
               ],
-            )))
+            ))))
         .toList();
     return Scaffold(
       body: Container(
@@ -449,7 +479,7 @@ class _RestaurantDetailsScreenWidgetState
               options: CarouselOptions(
                   autoPlay: true,
                   enlargeCenterPage: true,
-                  aspectRatio: 2.0,
+                  aspectRatio: 1.0,
                   onPageChanged: (index, reason) {
                     setState(() {
                       _offerCurrent = index;
@@ -457,26 +487,7 @@ class _RestaurantDetailsScreenWidgetState
                   }),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _offerController.animateToPage(entry.key),
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_offerCurrent == entry.key ? 0.9 : 0.4)),
-                ),
-              );
-            }).toList(),
-          ),
+        
           const SizedBox(
             height: 20,
           ),
